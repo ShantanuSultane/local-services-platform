c3bcf239aa7feacebafd54c9edfc390b1f7531e9
Shantanu Sultane <shantanusultane49@gmail.com>
2026-02-16 02:46:46 +0530

    Implemented clean routing architecture with prop-driven state injection, modular route definitions, and optimized navigation flow for service booking and user authentication.

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "../components/ProtectedRoute";
+
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
 import Services from "../pages/Services";
@@ -13,52 +15,43 @@ import Register from "../pages/Register";
 import ForgotPassword from "../pages/ForgotPass";
 import ResetPassword from "../pages/ResetPass";
 
-// ❗ FIX: receive all props correctly
 export default function AppRouter({ bookings, addBooking, cancelBooking }) {
   return (
     <BrowserRouter>
       <MainLayout>
         <Routes>
-
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
 
-          {/* Services */}
           <Route path="/services" element={<Services />} />
           <Route
             path="/services/:id"
             element={<ServiceDetails onBook={addBooking} />}
           />
 
-          {/* Book page */}
           <Route
             path="/book/:id"
             element={<BookService addBooking={addBooking} />}
           />
 
-          {/* All bookings */}
+          <Route path="/providers" element={<Providers />} />
+          
           <Route
-            path="/bookings"
-            element={
-              <Bookings
-                bookings={bookings}
-                onCancelBooking={cancelBooking}
-              />
-            }
-          />
+  path="/bookings"
+  element={
+    <ProtectedRoute>
+      <Bookings bookings={bookings} onCancelBooking={cancelBooking} />
+    </ProtectedRoute>
+  }
+/>
 
-          {/* Users */}
+
+          {/* AUTH ROUTES */}
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password" element={<ResetPassword />} />
-
-          {/* Providers */}
-          {/* <Route path="/providers" element={<Providers />} /> */}
-
-          <Route path="/providers" element={<Providers />} />
-
 
         </Routes>
       </MainLayout>
